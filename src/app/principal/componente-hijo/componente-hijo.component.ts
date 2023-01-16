bc81fe856de80ca88d1554a6c6d1b395dbc0b51e
kathycrv14 <katherinecristinarv@outlook.com>
2023-01-16 01:30:04 -0500

    Mensaje de commit

---
+++ src/app/principal/componente-hijo/componente-hijo.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-hijo',
+  templateUrl: './componente-hijo.component.html',
+  styleUrls: ['./componente-hijo.component.css']
+})
+export class ComponenteHijoComponent {
+
+  @Input() mensaje_padre: string ="";
+
+}
---
+++ src/app/principal/componente-padre/componente-padre.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-padre',
+  templateUrl: './componente-padre.component.html',
+  styleUrls: ['./componente-padre.component.css']
+})
+export class ComponentePadreComponent {
+
+// Ejemplo 01- Comunicacion Padre-hijo
+datos_padre : string =""; //atributo
+
+}
+
